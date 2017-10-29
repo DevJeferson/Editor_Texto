f6712dff87c7b4e1515c5343a20f34994e9d8066
Jeferson Machado da Silva <jeffy1996@outlook.com>
2017-10-29 18:51:59 -0200

    i

--- Editor-Texto (v1).py
+++ Editor-Texto (v1).py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 from universe import *
 
-
-"testessssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
 "========Developers: Jeferson e Edgar=========="
 
 '''========================================EDITOR-DE-TEXTO-AVANÇADO(Versão 1.0)======================================'''
